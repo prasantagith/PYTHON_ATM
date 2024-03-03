27c199f83c782a635bb2b937e5b773df7bbd2898
prasantagith <prasantasethi2002@gmail.com>
2024-03-03 21:13:59 +0530

    first commit

---
+++ ATM code.py
@@ -0,0 +1,31 @@
+
+pin=int(input('create a pin:'))
+balance=30000
+
+print("Welcome to HDFC")
+for i in range(3):
+    pinno=int(input("enter your pin number:"))
+    if pinno==pin:
+        print("Deposit : d  Withdrawl : w ")
+        option=input("choose the option")
+        if option=="d":
+            depositamount=int(input("Enter the amount you want to deposit:"))
+            print("open the box and it will collect your amount")
+            print("Amount deposited successfully")
+            break
+        else:
+            withdrawlamount=int(input("enter the amount you want to withdraw:"))
+            if withdrawlamount<=balance:
+                
+                print("Amount withdrawn successfully")
+                break
+            else:
+                print("insufficient funds")
+    
+    
+        
+    
+    else:
+        print("your pin is incorrect")
+else:
+    print("card blcoked.")
